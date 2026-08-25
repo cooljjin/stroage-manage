1839d05985f3fe9edf8a1cfa1b7d79bd77394848
jinkim <jich980611@gmail.com>
2026-08-25 14:51:10 +0900

    ci: restore reproducible Xcode Cloud dependencies

--- ios/App/ci_scripts/ci_post_clone.sh
+++ ios/App/ci_scripts/ci_post_clone.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-set -e
+set -eux
 
-brew install node
+HOMEBREW_NO_AUTO_UPDATE=1 brew install node
+export PATH="$(brew --prefix node)/bin:$PATH"
 
 cd "$CI_WORKSPACE"
+node --version
+npm --version
 npm ci
 npm run ios:prepare
 
